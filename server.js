f202d178d1f116c0bf97347bf33d4eb33e8b7367
Lim Chaemin <wldnjscoals@gmail.com>
2022-09-30 23:37:53 +0900

    node.js-study

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require('express')
 const app = express()
 app.use(express.urlencoded({extended: true}))
 const MongoClient = require('mongodb').MongoClient
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
+
 app.set('view engine', 'ejs')
 
+app.use('/public', express.static('public'))
 
 var db
 MongoClient.connect('mongodb+srv://bolo:minwon0521@cluster0.hrkz1cc.mongodb.net/?retryWrites=true&w=majority', {useUnifiedTopology:true}, (error, client) => {
@@ -20,10 +24,10 @@ MongoClient.connect('mongodb+srv://bolo:minwon0521@cluster0.hrkz1cc.mongodb.net/
 
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+    res.render('index.ejs')
 })
 app.get('/write', function(req, res){
-    res.sendFile(__dirname + '/write.html')
+    res.render('write.ejs')
 })
 
 app.post('/add', (req, res) => {
@@ -48,11 +52,31 @@ app.get('/list', (req, res) => {
     })
 })
 
-app.delete('/delete', (req, res) => {
+app.delete('/delete/:id', (req, res) => {
     req.body._id = parseInt(req.body._id)
     console.log(req.body)
     db.collection('post').deleteOne(req.body, (error, result) => {
         console.log('삭제완료')
+        res.status(200).send({message : '성공함'})
     })
 })
 
+app.get('/detail/:id', (req, res) => {
+    db.collection('post').findOne({_id : parseInt(req.params.id)}, (error, result) => {
+        console.log(result)
+        res.render('detail.ejs', {data : result})
+        if(result == null){
+            res.status(404).send({message : '없엉'})
+        }
+    })
+})
+
+app.get('/edit/:id', (req, res) => {
+    db.collection('post').findOne({_id : parseInt(req.params.id)}, (error, result) => {
+        console.log(result)
+        res.render('edit.ejs', {post : result})
+        if(result == null){
+            res.status(404).send({message : '없엉'})
+        }
+    })
+})
