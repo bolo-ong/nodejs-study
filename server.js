5c0bc8a6945dce4d4b08db9fd5e160fedf83e9cf
Lim Chaemin <wldnjscoals@gmail.com>
2022-10-09 16:56:40 +0900

    method-override & PUT

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 const MongoClient = require('mongodb').MongoClient
 const methodOverride = require('method-override')
 app.use(methodOverride('_method'))
@@ -10,10 +10,10 @@ app.set('view engine', 'ejs')
 app.use('/public', express.static('public'))
 
 var db
-MongoClient.connect('mongodb+srv://bolo:minwon0521@cluster0.hrkz1cc.mongodb.net/?retryWrites=true&w=majority', {useUnifiedTopology:true}, (error, client) => {
+MongoClient.connect('mongodb+srv://bolo:minwon0521@cluster0.hrkz1cc.mongodb.net/?retryWrites=true&w=majority', { useUnifiedTopology: true }, (error, client) => {
     db = client.db('todoapp')
 
-    if(error){
+    if (error) {
         return console.log(error)
     }
 
@@ -26,20 +26,20 @@ MongoClient.connect('mongodb+srv://bolo:minwon0521@cluster0.hrkz1cc.mongodb.net/
 app.get('/', (req, res) => {
     res.render('index.ejs')
 })
-app.get('/write', function(req, res){
+app.get('/write', function (req, res) {
     res.render('write.ejs')
 })
 
 app.post('/add', (req, res) => {
     res.send('전송완료')
-    
-    db.collection('counter').findOne({name : '게시물 개수'}, (error, result) => {
+
+    db.collection('counter').findOne({ name: '게시물 개수' }, (error, result) => {
         let totalNotice = result.totalPost
-        
-        db.collection('post').insertOne({ _id : totalNotice + 1, 제목 : req.body.title, 날짜 : req.body.date}, (error, result) => {
+
+        db.collection('post').insertOne({ _id: totalNotice + 1, 제목: req.body.title, 날짜: req.body.date }, (error, result) => {
             console.log('저장완료')
-            db.collection('counter').updateOne({name : '게시물 개수'}, {$inc : {totalPost:1}}, (error, result) => {
-                if(error){return console.log(error)}
+            db.collection('counter').updateOne({ name: '게시물 개수' }, { $inc: { totalPost: 1 } }, (error, result) => {
+                if (error) { return console.log(error) }
             })
         })
     })
@@ -48,7 +48,7 @@ app.post('/add', (req, res) => {
 app.get('/list', (req, res) => {
     db.collection('post').find().toArray((error, result) => {
         console.log(result)
-        res.render('list.ejs', {posts : result})
+        res.render('list.ejs', { posts: result })
     })
 })
 
@@ -57,26 +57,33 @@ app.delete('/delete/:id', (req, res) => {
     console.log(req.body)
     db.collection('post').deleteOne(req.body, (error, result) => {
         console.log('삭제완료')
-        res.status(200).send({message : '성공함'})
+        res.status(200).send({ message: '성공함' })
     })
 })
 
 app.get('/detail/:id', (req, res) => {
-    db.collection('post').findOne({_id : parseInt(req.params.id)}, (error, result) => {
+    db.collection('post').findOne({ _id: parseInt(req.params.id) }, (error, result) => {
         console.log(result)
-        res.render('detail.ejs', {data : result})
-        if(result == null){
-            res.status(404).send({message : '없엉'})
+        res.render('detail.ejs', { data: result })
+        if (result == null) {
+            res.status(404).send({ message: '없엉' })
         }
     })
 })
 
 app.get('/edit/:id', (req, res) => {
-    db.collection('post').findOne({_id : parseInt(req.params.id)}, (error, result) => {
+    db.collection('post').findOne({ _id: parseInt(req.params.id) }, (error, result) => {
         console.log(result)
-        res.render('edit.ejs', {post : result})
-        if(result == null){
-            res.status(404).send({message : '없엉'})
+        res.render('edit.ejs', { post: result })
+        if (result == null) {
+            res.status(404).send({ message: '없엉' })
         }
+    })
+})
+
+app.put('/edit', (req, res) => {
+    db.collection('post').updateOne({ _id: parseInt(req.body.id)}, { $set: { 제목: req.body.title, 날짜: req.body.date}}, (error, result) => {
+        console.log('수정완료')
+        res.redirect('/list')
     })
 })
